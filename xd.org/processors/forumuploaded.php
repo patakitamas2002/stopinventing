2f95464e1667f5a64800892179bac559eb4d5355
patakitamas2002 <113434354+patakitamas2002@users.noreply.github.com>
2022-10-08 22:54:35 +0200

    Nem használt file

--- xd.org/processors/forumuploaded.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../mydbms.php';
-    $con = connect('viccoldal','root','');
-    $query="UPDATE post SET uploaded = 'true'  WHERE id=".$_POST['postId'];
-    $result = mysqli_query($con, $query) or die ("Nem sikerült ".$query);
-    header('Location: ../index.php');
-?>
