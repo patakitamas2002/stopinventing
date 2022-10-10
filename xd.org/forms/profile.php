7c0efbdd20d23d00541ecd2098ceb9271c286de7
BirGer05 <113984073+BirGer05@users.noreply.github.com>
2022-10-10 22:55:34 +0200

    Update profile.php

--- xd.org/forms/profile.php
+++ xd.org/forms/profile.php
@@ -14,58 +14,93 @@ id-jű  -->
     //echo $path;
     //echo $userdata[2]." test";
     if($userdata[1] != ""){
-        echo "<img src='$path' height=300>";
+        echo "<img class='profile' src='$path' height=300>";
     }
     else{
         $path = './default_pfp/'.$userdata[2].'.jpg';
-        echo "<img src=$path height=300>";
+        echo "<img class='profile' src=$path height=300>";
     }
     //echo "<img src = 'imgs/pfp/0cf4695592d3011073f3f31045137456.jpg'>";
 ?>
 <p id="error" style="color:red"></p>
 
-<link rel="stylesheet"  
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
     <title>MyProfile</title>
-    <style>
-    input {
-    display: block;
-    box-sizing: border-box;
-    }
-       
-    table {  
-        border-collapse: collapse;  
-    }  
-
-    .inline{   
-        display: inline-block;   
-               
-        margin: 20px 0px;   
-    }   
-         
-    input, button{   
-        height: 34px;   
-    }
-    </style> 
 
 <form name="updateForm" action="processors/updateprofile.php" method="POST" enctype="multipart/form-data" onsubmit="return JelszoCheck()">
     <input type="hidden" name="userId" value=<?=$_SESSION['id']?>>
-    Felhasználónév:
+    <p>Felhasználónév: </p>
     <input type="text" name="username" require value=<?=$_SESSION['username']?>><br>
 
-    Jelszó módosítása:
+    <p>Jelszó módosítása: </p>
     <input type="password" name="password" value="" require><br>
 
-    Jelszó megint:
+    <p>Jelszó megint: </p>
     <input type="password" name="passwordAgain" value="" require><br>
 
-    Profilkép frisstése: <input type="file" accept=".jpeg,.jpg,.png" name="pfp"/><br>
+    <p>Profilkép frisstése: </p>
+    <input type="file" accept=".jpeg,.jpg,.png" name="pfp"/><br>
 
-    <input type="submit" name="update" value="Frissít">
+    <input class="update" type="submit" name="update" value="Frissít">
 </form>
 <form name="updateForm" action="processors/deletepfp.php" method="POST" >
-    <input type="submit" name="deletepfp" value="Profilkép törlése">
+    <input class="del" type="submit" name="deletepfp" value="Profilkép törlése">
 </form>
+
+<style>   
+
+    body{
+        background-color: turquoise;
+    }
+
+    .update{
+        background-color:chartreuse;
+        border: 2px solid black;
+    }
+
+    .del{
+        color: white;
+        background-color:red;
+        border: 2px solid black;
+    }
+
+    input {
+        display: block;
+        box-sizing: border-box;
+    }
+    
+    p{
+        color: white;
+        font-size: large;
+    }
+    
+    .profile{
+        display: block;
+        margin: auto;
+        border: 2px solid black;
+        border-radius: 200px;
+    }
+     
+    input{   
+        height: 34px;
+        width: 100%;
+    }
+
+    button{
+        height: 34px;
+        margin: 0 auto;
+        background-color:chartreuse;
+        border: 2px solid black;
+    }
+
+    form {
+    margin: auto 40%;
+    padding: 50px;
+    border: 2px solid black;
+    border-radius: 50px;
+    background-color:royalblue;
+    }
+    </style> 
 
 <script>
     function JelszoCheck() {
