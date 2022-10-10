653a0c3dc4ed2fc9bf51e48982403cab66cbeba2
BirGer05 <113984073+BirGer05@users.noreply.github.com>
2022-10-10 22:55:08 +0200

    Update regform.php

--- xd.org/forms/regform.php
+++ xd.org/forms/regform.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet"  
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
-    <style>   
-    table {  
-        border-collapse: collapse;  
-    }  
-        .inline{   
-            display: inline-block;   
-               
-            margin: 20px 0px;   
-        }   
-         
-        input, button{   
-            height: 34px;   
-        }  </style> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
 <head>
     <meta charset="UTF-8">
     <title>Regisztráció</title>
+    <link rel="stylesheet" href="reg.css">
 </head>
 <body>
-    <h1>Regisztráció</h1>
     <form action="../processors/register.php" method="POST" enctype="multipart/form-data">
-        Email*: <input type="email" name="email" required placeholder="example@host.com"><br>
-        Felhasználónév*: <input type="text" name="username" required placeholder="Username"><br>
-        Jelszó*: <input type="password" name="password" placeholder="Password" required><br>
-        Profilkép feltöltése: <input type="file" accept=".jpeg,.jpg,.png" name="pfp"><br>
-        <input type="submit" name="submit" value="Regisztrálok">
+        <h1>Regisztráció</h1>
+        <p>Email*:</p> <input type="email" name="email" required placeholder="example@host.com"><br>
+        <p>Felhasználónév*:</p> <input type="text" name="username" required placeholder="Username"><br>
+        <p>Jelszó*:</p> <input type="password" name="password" placeholder="Password" required><br>
+        <p>Profilkép feltöltése:</p> <input type="file" accept=".jpeg,.jpg,.png" name="pfp"><br>
+        <p>Jogosultság*:</p><br>
+        <select name="perms" required>
+            <option selected hidden disabled>Válasszon...</option>
+            <option value="user">Felhasználó</option> 
+            <option value="mod">Adminisztrátor</option>
+        </select><br>
+        <input class="reg" type="submit" name="submit" value="Regisztrálok" />
     </form>
     
 </body>
