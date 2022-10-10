42445310df5ca8b4587a8143e89abe93c5b9a339
BirGer05 <113984073+BirGer05@users.noreply.github.com>
2022-10-10 22:57:20 +0200

    Update loginform.php

--- xd.org/forms/loginform.php
+++ xd.org/forms/loginform.php
@@ -1,54 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet"  
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
     <style>   
-    table {  
-        border-collapse: collapse;  
-    }  
-    .inline{   
-        display: inline-block;   
-               
-        margin: 20px 0px;   
-    }
     
-    input {
-    display: block;
-    box-sizing: border-box;
-    }
-         
-    input, button{   
-        height: 34px;   
-    }
-
-    html,body {
-        height:100%;
-        width:100%;
-        margin:0;
-    }
-    body {
-    display:flex;
-    }
-    form {
-    margin:auto;
-    }
     </style> 
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    
     <form action="../processors/login.php" method="post">
-    <a href="../index.php?page=0">Jaj, mégse szeretnék bejelentkezni</a><br>
-        Felhasználónév:
-        <input type="text" name="username">
+        <p>Felhasználónév:</p>
+        <input class="text" type="text" name="username">
         <br>
-        Jelszó:
-        <input type="password" name="password">
+        <p>Jelszó:</p>
+        <input class="text" type="password" name="password">
         <br><br>
-        <input type="submit" value="Bejelentkezés" name="login">
-        
+        <input class="btn" type="submit" value="Bejelentkezés" name="login">
+        <a class="nono" href="../index.php?page=0">Jaj, mégse szeretnék bejelentkezni</a>
         <button><a href ="regform.php">Regisztráció</a></button>
     </form>
     
