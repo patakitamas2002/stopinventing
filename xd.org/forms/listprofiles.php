c51d3cc02e3baa248c2069efcacde2b58174d247
BirGer05 <113984073+BirGer05@users.noreply.github.com>
2022-10-10 22:57:00 +0200

    Update listprofiles.php

--- xd.org/forms/listprofiles.php
+++ xd.org/forms/listprofiles.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet"  
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">   
-    <style>   
-    table {  
-        border-collapse: collapse;  
-    }  
-        .inline{   
-            display: inline-block;   
-               
-            margin: 20px 0px;   
-        }   
-         
-        input, button{   
-            height: 34px;   
-        }  </style> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profilok</title>
+    <link rel="stylesheet" href="listp.css">
 </head>
 <body>
 <table>  
